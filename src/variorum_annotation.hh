20ac3d0f0db6870c78a43504c75ea1e5993da895
Naman Kulshreshtha <nkulshr@clemson.edu>
2024-04-22 09:53:19 -0700

    Added energy

--- src/variorum_annotation.hh
+++ src/variorum_annotation.hh
@@ -4,21 +4,21 @@
 ///@brief store the caller function's file, line and name.
 #define CALL_INFO __FILE__, __LINE__, __func__
 ///@brief macro wrapper for actual functions, needed to store the call info.
-#define VARIORUM_ANNOTATE_GET_NODE_POWER_JSON                                  \
-  variorum_annotate_get_node_power_json(CALL_INFO)
+#define VARIORUM_ANNOTATE_GET_POWER_INFO                                       \
+  variorum_annotate_function_call(VARIORUM_POWER_CALL, CALL_INFO)
 ///@brief macro wrapper for actual functions, needed to store the call info.
-#define VARIORUM_ANNOTATE_GET_NODE_POWER_DOMAIN_INFO_JSON                      \
-  variorum_annotate_get_node_power_domain_info_json(CALL_INFO)
-///@brief wrapper for variorum node power json API.
-///The Function writes the result in a json format 
-///to a filename after the hostname.
-void variorum_annotate_get_node_power_json(const char *file, int line,
-                                           const char *function_name);
-///@brief wrapper for variorum node power domain info json API.
-///The Function writes the result in a json format 
-///to a filename after the hostname.
-void variorum_annotate_get_node_power_domain_info_json(
-    const char *file, int line, const char *function_name);
+#define VARIORUM_ANNOTATE_GET_NODE_POWER_DOMAIN_INFO                           \
+  variorum_annotate_function_call(VARIORUM_DOMAIN_CALL, CALL_INFO)
+#define VARIORUM_ANNOTATE_GET_ENERGY_INFO                                      \
+  variorum_annotate_function_call(VARIORUM_ENERGY_CALL, CALL_INFO)
+typedef enum {
+  VARIORUM_POWER_CALL,
+  VARIORUM_ENERGY_CALL,
+  VARIORUM_DOMAIN_CALL,
+} variorum_call_type_t;
+
+void variorum_annotate_function_call(variorum_call_type_t type, const char *calling_file,
+                            int line, const char *function_name);
 
 void variorum_annotate_init();
 
